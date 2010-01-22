1944cbd6fb420f8fb110687488b3b3a96e9964cd
derekja <derekja@5231ca88-fc88-11de-9b1c-9fe8246cf834>
2010-01-22 01:27:36 +0000

    This is a checkin of servomotor code. The current state of the playground is that both fan and servo are run from the same joystick input into analog 0, the motor comes off of pwm 2 and 3, the servo is on pwm10.

--- trunk/test2/main1.cpp
+++ trunk/test2/main1.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "WProgram.h"
+//#include "Servo.h"
 // the next two lines are needed to hack around a serial monitor bug
 extern "C" void __cxa_pure_virtual(void);
 void __cxa_pure_virtual(void) {}
@@ -14,18 +15,27 @@ int ledPin =  13;    // LED connected to digital pin 13
 int motor1Pin1 = 2;
 int motor1Pin2 = 3;
 int pot = 0;
+//Servo servoMotor;
+int servoPin = 10;
+int servoValue = 0;
+long lastPulse = 0;    // the time in milliseconds of the last pulse
+int refreshTime = 20; // the time needed in between pulses
+
 
  // The setup() method runs once, when the sketch starts
 
  void setup()   {
 	 // set up serial
-	 Serial.begin( 9600 );
+	 Serial.begin( 57600 );
 	 Serial.print("start");
    // initialize the digital pin as an output:
    pinMode(ledPin, OUTPUT);
    // init the pot as an input
    pinMode(pot, INPUT);
    //PWM ports go output by default, so don't need to set that
+   pinMode(servoPin, OUTPUT);
+   //attach the servo
+   //servoMotor.attach(servoPin);
  }
 
  // the loop() method runs over and over again,
@@ -34,8 +44,8 @@ int pot = 0;
  int getPot() {
 	 int v;
 	 v = analogRead(pot) / 4;
-	 Serial.print( v );
-	 Serial.println();
+	 //Serial.print( v );
+	 //Serial.println();
 	 return v;
  }
 
@@ -44,6 +54,10 @@ int pot = 0;
 	 int d = 0; // initial var for which motor pin
 	 int s = 0; // initial var for speed
 	 int v = getPot();
+	 //Serial.print(servoValue);
+	 servoValue = map(v, 0, 255, 500, 5000);
+	 Serial.print(servoValue);
+	 Serial.println();
 	 if (v<80) {
 		 d = 1;
 		 s = 255-(v*2);
@@ -66,7 +80,18 @@ int pot = 0;
 	 default :
 		 analogWrite(motor1Pin1, 0);
 		 analogWrite(motor1Pin2, 0);
+		 servoValue = 2397;
 	 }
+	 //servoMotor.write(servoValue);
+
+	  // pulse the servo again if rhe refresh time (20 ms) have passed:
+	  if (millis() - lastPulse >= refreshTime) {
+	    digitalWrite(servoPin, HIGH);   // Turn the motor on
+	    delayMicroseconds(servoValue);       // Length of the pulse sets the motor position
+	    digitalWrite(servoPin, LOW);    // Turn the motor off
+	    lastPulse = millis();           // save the time of the last pulse
+	  }
+
    //analogWrite(motor1Pin1, 255);
 	// analogWrite(motor1Pin1, 0);
 	// analogWrite(motor1Pin2, 255);
@@ -74,7 +99,7 @@ int pot = 0;
 	// analogWrite(motor1Pin1, 128);
 	// analogWrite(motor1Pin2, 0);
    //digitalWrite(ledPin, LOW);    // set the LED off
-	// delay(1000);                  // wait for a second
+	 //delay(1000);                  // wait for a second
 	 // test2
  }
 
@@ -82,7 +107,7 @@ int pot = 0;
  init();
  setup();
  digitalWrite(ledPin, HIGH);   // set the LED on
-
+ //lastPulse = millis();
  for(;;) {
 loop();
  }

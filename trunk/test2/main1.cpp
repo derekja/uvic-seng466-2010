13459aac38bb6ba94da66cbc0695151e671c4b25
derekja <derekja@5231ca88-fc88-11de-9b1c-9fe8246cf834>
2010-01-26 19:58:22 +0000

    version to show mantis.. set TCCR4 first! if after set OCR4x then no high byte

--- trunk/test2/main1.cpp
+++ trunk/test2/main1.cpp
@@ -5,110 +5,51 @@
  *      Author: derekja
  */
 
-#include "WProgram.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+//#include "WProgram.h"
 #include "Servotmp.h"
+
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 // the next two lines are needed to hack around a serial monitor bug
 extern "C" void __cxa_pure_virtual(void);
 void __cxa_pure_virtual(void) {}
-
-int ledPin =  13;    // LED connected to digital pin 13
-int motor1Pin1 = 2;
-int motor1Pin2 = 3;
-int pot = 0;
-int servoPin = 11;
-unsigned int servoValue = 0;
-long lastPulse = 0;    // the time in milliseconds of the last pulse
-int refreshTime = 20; // the time needed in between pulses
 
 
  // The setup() method runs once, when the sketch starts
 
  void setup()   {
 	 // set up serial
-	 Serial.begin( 57600 );
-	 Serial.print("start");
-   // initialize the digital pin as an output:
-   pinMode(ledPin, OUTPUT);
-   // init the pot as an input
-   pinMode(pot, INPUT);
+	 //Serial.begin( 57600 );
+	 //Serial.print("start");
+
    //PWM ports go output by default, so don't need to set that
    //pinMode(servoPin, OUTPUT);
-   //attach the servo
-   delay(5000);
-   Serial.print("start");
+   //Serial.print("start");
    servoInit();
  }
 
  // the loop() method runs over and over again,
  // as long as the Arduino has power
 
- int getPot() {
-	 int v;
-	 v = analogRead(pot) / 4;
-	 //Serial.print( v );
-	 //Serial.println();
-	 return v;
- }
-
  void loop()
  {
-	 int d = 0; // initial var for which motor pin
-	 int s = 0; // initial var for speed
-	 int v = getPot();
-	 //Serial.print(servoValue);
-	 servoValue = map(v, 0, 255, 500, 5000);
-	 //Serial.print(servoValue);
-	 //Serial.println();
-	 if (v<80) {
-		 d = 1;
-		 s = 255-(v*2);
-	 }
-	 if (v>110) {
-		 d = 2;
-		 s = v;
-	 }
-	 switch (d) {
-	 case 1 :
-		 analogWrite(motor1Pin1, s);
-		 analogWrite(motor1Pin2, 0);
-		 break;
 
-	 case 2 :
-		 analogWrite(motor1Pin1, 0);
-		 analogWrite(motor1Pin2, s);
-		 break;
+	 servoSet(0);
 
-	 default :
-		 analogWrite(motor1Pin1, 0);
-		 analogWrite(motor1Pin2, 0);
-		 servoValue = 2397;
-	 }
-	 servoSet(servoValue);
+	 //delay(500);                  // wait for a second
 
-	  // pulse the servo again if rhe refresh time (20 ms) have passed:
-	  //if (millis() - lastPulse >= refreshTime) {
-	  //  digitalWrite(servoPin, HIGH);   // Turn the motor on
-	  //  delayMicroseconds(servoValue);       // Length of the pulse sets the motor position
-	  //  digitalWrite(servoPin, LOW);    // Turn the motor off
-	  //  lastPulse = millis();           // save the time of the last pulse
-	 // }
-
-   //analogWrite(motor1Pin1, 255);
-	// analogWrite(motor1Pin1, 0);
-	// analogWrite(motor1Pin2, 255);
-	 delay(1000);                  // wait for a second
-	// analogWrite(motor1Pin1, 128);
-	// analogWrite(motor1Pin2, 0);
-   //digitalWrite(ledPin, LOW);    // set the LED off
-	 //delay(1000);                  // wait for a second
-	 // test2
  }
 
  int main() {
- init();
+ //init();
+		//sbi(TCCR4A, WGM40);
+	    //TCCR4A = 0b000001;    // COM4B1, COM4B0, COM4C1, COM4C0, WGM41, and WGM40
+
  setup();
- digitalWrite(ledPin, HIGH);   // set the LED on
  //lastPulse = millis();
  for(;;) {
 loop();
--- trunk/test2/servotmp.cpp
+++ trunk/test2/servotmp.cpp
@@ -2,29 +2,29 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "servotmp.h"
-#include "WProgram.h"
+//#include "WProgram.h"
 
 void servoInit() {
 
 	unsigned char sreg;
 
-
+    TCCR4A = 0b000001;    // COM4B1, COM4B0, COM4C1, COM4C0, WGM41, and WGM40
 	//set OCR4A to top (just using an arbitrary value for now)
     sreg = SREG;
     cli();
-    OCR4A = 0x03FF;
+    OCR4A = 0x09FF;
     SREG = sreg;
 
 	//set ICR4 to top (just using an arbitrary value for now)
-    //sreg = SREG;
-    //cli();
-    //ICR4 = 0x03FC;
-    //SREG = sreg;
+    sreg = SREG;
+    cli();
+    ICR4 = 0x06FC;
+    SREG = sreg;
 
     //put a comparator value in OCR4B (again, arbitrary for now)
     sreg = SREG;
     cli();
-    OCR4B = 0x0051;
+    OCR4B = 0x0352;
     SREG = sreg;
 
     //set registers - mode 15, fast PWM, non-inverted
@@ -37,8 +37,9 @@ void servoInit() {
 	//TCCR4B |= ( _BV(CS40));
 	//TCCR4B &= ~(_BV(CS42) | _BV(CS41));
 
-    TCCR4A = 0b100001;    // COM4B1, COM4B0, COM4C1, COM4C0, WGM41, and WGM40
-    TCCR4B = 0b01001;     // WGM43, WGM42, CD42, CS41, and CS40
+    TCCR4A = 0b000001;    // COM4B1, COM4B0, COM4C1, COM4C0, WGM41, and WGM40
+    TCCR4A = 0b100011;    // COM4B1, COM4B0, COM4C1, COM4C0, WGM41, and WGM40
+    TCCR4B = 0b11001;     // WGM43, WGM42, CD42, CS41, and CS40
 
     //enable interrupts
 	//TIMSK4 |= (_BV(OCIE4B));
@@ -46,8 +47,8 @@ void servoInit() {
 	// set PWM7 to an output pin (OC4B), PH4
 	DDRH |= (_BV(DDH4));
 
-	Serial.print("servoInit done");
-	Serial.println();
+	//Serial.print("servoInit done");
+	//Serial.println();
 	return;
 }
 
@@ -62,13 +63,13 @@ void servoSet(unsigned int value) {
 	cli();
 	//OCR4B = value;
 	tmp = OCR4B;
-	tmp1 = OCR4A;
+	tmp1 = ICR4;
 	SREG = sreg;
 
-	Serial.print("servoSet done: ");
-	Serial.print(tmp);
-	Serial.print("  ");
-	Serial.print(tmp1);
-	Serial.println();
+	//Serial.print("servoSet done: ");
+	//Serial.print(tmp);
+	//Serial.print("  ");
+	//Serial.print(tmp1);
+	//Serial.println();
 	return;
 }

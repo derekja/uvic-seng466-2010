ce8f3599f4c7295e84c795cbf5e0c98ecb7fac09
fieranmason <fieranmason@5231ca88-fc88-11de-9b1c-9fe8246cf834>
2010-04-09 19:25:09 +0000

    Control compiles this set of commits will be for the initial creation of the control system which was inspired by midterm 2

--- SENG466RemoteStation/Control.cpp
+++ SENG466RemoteStation/Control.cpp
@@ -6,130 +6,126 @@
  */
 
 #include "Control.h"
+#include "Arduino/WProgram.h"
+#include "common.h"
 
-int newFanVal;
-int newDirVal;
-extern int fanVal;
-extern int dirVal;
-extern int dir;
-extern int speed;
-extern int turn;
+/*prototypes*/
+uint16_t sonar( int sonar );
+int yaw();
 
-//void control()
-//{
-//	while( true )
-//	{
-//		//set fan position
-//		if ( fanPos < 750 )
-//		{
-//			newFanVal = map( fanPos, 120, 750, 0, 255 );
-//			dir = 2;
-//			newFanVal = 255 - newFanVal;
-//		}
-//
-//		if ( fanPos > 800 )
-//		{
-//			newFanVal = map( fanPos, 800, 1023, 0, 255 );
-//			dir = 1;
-//		}
-//
-//		//set direction
-//		if ( dirPos < 425 )
-//		{
-//			newDirVal = map( dirPos, 0, 450, 1000, 0 );
-//			fanVal = ( fanVal * dirVal ) / 1000;
-//		}
-//
-//		if ( dirPos > 500 )
-//		{
-//			newDirVal = map( dirPos, 500, 1023, 1000, 0 );
-//			newFanVal = ( newFanVal * newDirVal ) / 1000;
-//		}
-//
-//		//age old data
-//		fanVal = ( ( fanVal * 2 ) / 3 ) + ( ( newFanVal * 1 ) / 3 );
-//		dirVal = ( ( dirVal * 2 ) / 3 ) + ( ( newDirVal * 1 ) / 3 );
-//
-//		//wait for next call
-//		task_next();
-//	}
-//}
-
-void control() {
-	const int STOP = 0;
-	const int FOLLOW_WALL = 1;
-	const int TURN = 2;
-	const int AVOID_OBSTACLE = 3;
-	const int FOLLOW_WALL_SET_POINT = 24;
-	const int YAW_SET_POINT = 0;
-
-	int follow_wall_prev_error;
-	int follow_wall_error;
-	int follow_wall_p;
-	int follow_wall_kp = 8;
-	int follow_wall_d;
-	int follow_wall_kd = 4;
-	int follow_wall_i;
-	int follow_wall_ki = 2;
-	int follow_wall_turn;
-
-	int yaw_prev_error;
-	int yaw_error;
-	int yaw_p;
-	int yaw_kp = 8;
-	int yaw_d;
-	int yaw_kd = 4;
-	int yaw_i;
-	int yaw_ki = 2;
-	int yaw_turn;
-
-	while (true) {
-		switch (state) {
-		case STOP:
-			state = command;
-			break;
-		case FOLLOW_WALL:
-			state = command;
-			speed = SPEED;
-
-			yaw_error = yaw() - YAW_SETPOINT;
-			yaw_p = yaw_error;
-			yaw_d = yaw_error - yaw_prev_error;
-			yaw_i = (2 * yaw_old_error) / 3 + yaw_error / 3;
-			yaw_prev_error = yaw_error;
-			yaw_old_error = yaw_i;
-			yaw_turn = (yaw_kp * yaw_p + yaw_kd * yaw_d + yaw_ki * yaw_i) / X;//that scaling factor of 1/X should keep the value between 100 and -100
+int speed;
+int turn;
 
 
-			follow_wall_error = sonar() - FOLLOW_WALL_SET_POINT;
-			follow_wall_p = follow_wall_error;
-			follow_wall_d = follow_wall_error - follow_wall_prev_error;
-			follow_wall_i = (2 * follow_wall_old_error) / 3 + follow_wall_error
-					/ 3;
-			follow_wall_prev_error = follow_wall_error;
-			follow_wall_old_error = follow_wall_i;
-			follow_wall_turn = (follow_wall_kp * follow_wall_p + follow_wall_kd
-					* follow_wall_d + follow_wall_ki * follow_wall_i) / 3;//that scaling factor of 1/3 should keep the value between 100 and -100
+extern int command;
 
-			turn = (2 * yaw_turn + follow_wall_turn) / 3;
+int state;
 
-			break;
-//		case TURN:
-//			speed = 0;
-//			if( yaw says we've stopped)
-//			{
-//				turn = 50;
-//				delay(1000);//wait for a bit while we reorient
-//				while( !( sonar( FRONT ) > OBSTACLE_AVOIDANCE_THREASHOLD ) {
-//							turn = 0;
-//							speed = SPEED;
-//							while (sonar(RIGHT) > FOLLOW_WALL_SET_POINT)
-//							;
-//							state = FOLLOW_WALL;
-//						}
-//						break;
-//					}
+const int FOLLOW_WALL_SET_POINT = 24;
+const int FOLLOW_WALL_SPEED = 100;
+
+int follow_wall_prev_error;
+int follow_wall_error;
+int follow_wall_old_error;
+int follow_wall_p;
+int FOLLOW_WALL_KP = 8;
+int follow_wall_d;
+int FOLLOW_WALL_KD = 4;
+int follow_wall_i;
+int FOLLOW_WALL_KI = 2;
+int follow_wall_turn;
+
+const int YAW_SETPOINT = 0;
+
+int yaw_prev_error;
+int yaw_error;
+int yaw_old_error;
+int yaw_p;
+int yaw_kp = 8;
+int yaw_d;
+int yaw_kd = 4;
+int yaw_i;
+int yaw_ki = 2;
+int yaw_turn;
+
+void control()
+{
+		switch( state )
+		{
+			case STOP:
+				state = command;
+				break;
+			case FOLLOW_WALL:
+
+				if( yaw() > 0 )
+				{
+					yaw_error = yaw() - YAW_SETPOINT;
+				}
+				else
+				{
+					yaw_error = - ( -yaw() - YAW_SETPOINT );
+				}
+				yaw_p = yaw_error;
+				yaw_d = yaw_error - yaw_prev_error;
+				yaw_i = ( 2 * yaw_old_error )/3 + yaw_error/3;
+				yaw_prev_error = yaw_error;
+				yaw_old_error = yaw_i;
+				yaw_turn = ( yaw_kp*yaw_p
+								+ yaw_kd*yaw_d
+								+ yaw_ki*yaw_i )/1600;//that scaling factor of 1/X should keep the value between 100 and -100
+
+
+				follow_wall_error = sonar( RIGHT_SONAR ) - FOLLOW_WALL_SET_POINT;
+				follow_wall_p = follow_wall_error;
+				follow_wall_d = follow_wall_error - follow_wall_prev_error;
+				follow_wall_i = ( 2 * follow_wall_old_error )/3 + follow_wall_error/3;
+				follow_wall_prev_error = follow_wall_error;
+				follow_wall_old_error = follow_wall_i;
+				follow_wall_turn = ( FOLLOW_WALL_KP*follow_wall_p
+										+ FOLLOW_WALL_KD*follow_wall_d
+										+ FOLLOW_WALL_KI*follow_wall_i )/3;//that scaling factor of 1/3 should keep the value between 100 and -100
+
+				turn = ( 2*yaw_turn + follow_wall_turn )/3;
+//				turn = follow_wall_turn;
+				speed = FOLLOW_WALL_SPEED;
+				state = command;
+				break;
+//			case TURN:
+//				speed = 0;
+//				if( yaw says we've stopped)
+//				{
+//					turn = 50;
+//					delay( 1000 );//wait for a bit while we reorient
+//					while( !( sonar( FRONT ) > OBSTACLE_AVOIDANCE_THREASHOLD );
+//					turn = 0;
+//					speed = SPEED;
+//					while( sonar( RIGHT ) > FOLLOW_WALL_SET_POINT );
+//					state = FOLLOW_WALL;
 //				}
-			}
+//				break;
+			default:
+				break;
 		}
+  }
+
+uint16_t sonar( int sonar )
+{
+	switch( sonar )
+	{
+		case FRONT_SONAR:
+			return 60;
+		case RIGHT_SONAR:
+			Serial.println( "Called sonar( RIGHT )" );
+			return 24;
+		case LEFT_SONAR:
+			return 24;
+		default:
+			return -1;
+	}
 }
+
+int yaw()
+{
+	return 0;
+}
+

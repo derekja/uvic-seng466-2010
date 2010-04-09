05ab545196a4a2a830c339cc4073d0bb731d5eaf
fieranmason <fieranmason@5231ca88-fc88-11de-9b1c-9fe8246cf834>
2010-04-09 19:24:33 +0000

    Actuate compiles this set of commits will be for the initial creation of the control system which was inspired by midterm 2

--- SENG466RemoteStation/Actuate.h
+++ SENG466RemoteStation/Actuate.h
@@ -8,6 +8,14 @@
 #ifndef ACTUATE_H_
 #define ACTUATE_H_
 
+static int LEFT_MOTOR_PIN_1 = 8;
+static int LEFT_MOTOR_PIN_2 = 9 ;
+static int LEFT_MOTOR_PIN_E = 10;
+
+static int RIGHT_MOTOR_PIN_1 = 11;
+static int RIGHT_MOTOR_PIN_2 = 12;
+static int RIGHT_MOTOR_PIN_E = 13;
+
 void actuate();
 
 #endif /* ACTUATE_H_ */

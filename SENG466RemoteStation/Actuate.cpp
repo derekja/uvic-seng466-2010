41e14eab1245cb8db61bddb33cf1dbfb57208c48
fieranmason <fieranmason@5231ca88-fc88-11de-9b1c-9fe8246cf834>
2010-04-09 19:24:17 +0000

    Actuate - compiles this set of commits will be for the initial creation of the control system which was inspired by midterm 2

--- SENG466RemoteStation/Actuate.cpp
+++ SENG466RemoteStation/Actuate.cpp
@@ -8,32 +8,75 @@
 #include "Actuate.h"
 #include "WProgram.h"
 
-static int motor1Pin1 = 11;
-static int motor1Pin2 = 12;
+int old_left_propulsion;
+int old_right_propulsion;
 
-void actuate() {
-	switch (d) {
-	case 1:
-		analogWrite(motor1Pin1, fanVal);
-		analogWrite(motor1Pin2, 0);
-		analogWrite(motor2Pin1, fanVal1);
-		analogWrite(motor2Pin2, 0);
-		break;
+int left_propulsion;
+int right_propulsion;
 
-	case 2:
-		analogWrite(motor1Pin1, 0);
-		analogWrite(motor1Pin2, fanVal);
-		analogWrite(motor2Pin1, 0);
-		analogWrite(motor2Pin2, fanVal1);
-		break;
+int left_propulsion_scaled;
+int right_propulsion_scaled;
 
-	default:
-		analogWrite(motor1Pin1, 0);
-		analogWrite(motor1Pin2, 0);
-		analogWrite(motor2Pin1, 0);
-		analogWrite(motor2Pin2, 0);
+extern int turn;
+extern int speed;
+
+void actuate()
+{
+	if( turn > 0 )
+	{
+		right_propulsion = speed - ( 2 * turn * speed ) / 100;//set value
+		right_propulsion = ( 2 * old_right_propulsion + right_propulsion ) / 3;//damp value
+		old_right_propulsion = right_propulsion;
+		right_propulsion_scaled = ( right_propulsion * 255 ) / 100;//scale value
+		left_propulsion = speed;
+		left_propulsion = ( 2 * old_left_propulsion + left_propulsion ) / 3;
+		left_propulsion_scaled = ( left_propulsion * 255 ) / 100;//scale value
+		old_left_propulsion = left_propulsion;
 	}
+	else
+	{
+		right_propulsion = speed;
+		right_propulsion = ( 2 * old_right_propulsion + right_propulsion ) / 3;//damp value
+		right_propulsion_scaled = ( right_propulsion * 255 ) / 100;//scale value
+		old_right_propulsion = right_propulsion;
 
-	//wait for next cycle
+		left_propulsion = speed - ( 2 * turn * speed ) / 100;//set value
+		left_propulsion = ( 2 * old_left_propulsion + left_propulsion ) / 3;//damp value
+		left_propulsion_scaled = ( left_propulsion * 255 ) / 100;//scale value
+		old_left_propulsion = left_propulsion;
+	}
+	if( left_propulsion_scaled == 0 && right_propulsion_scaled == 0)
+	{
+		digitalWrite( LEFT_MOTOR_PIN_E, 0 );
+		digitalWrite( RIGHT_MOTOR_PIN_E, 0 );
+		return;
+	}
+	else
+	{
+		if( left_propulsion_scaled > 0 )
+		{
+			digitalWrite( LEFT_MOTOR_PIN_1, 1 );
+			digitalWrite( LEFT_MOTOR_PIN_2, 0 );
+		}
+		else
+		{
+			digitalWrite( LEFT_MOTOR_PIN_1, 0 );
+			digitalWrite( LEFT_MOTOR_PIN_2, 1 );
+		}
+
+		analogWrite( LEFT_MOTOR_PIN_E, left_propulsion_scaled );
+
+		if( right_propulsion_scaled > 0 )
+		{
+			digitalWrite( RIGHT_MOTOR_PIN_1, 1 );
+			digitalWrite( RIGHT_MOTOR_PIN_2, 0 );
+		}
+		else
+		{
+			digitalWrite( RIGHT_MOTOR_PIN_1, 0 );
+			digitalWrite( RIGHT_MOTOR_PIN_2, 1 );
+		}
+
+		analogWrite( RIGHT_MOTOR_PIN_E, right_propulsion_scaled );
+	}
 }
-
